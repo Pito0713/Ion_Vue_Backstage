9462ff73cce15b56c7a986271a64b9161f87d319
Pito0713 <xli30334@gmail.com>
2023-11-21 16:03:39 +0800

    Update headerBarPage.vue
    測試gitHud

--- src/components/header-bar/headerBarPage.vue
+++ src/components/header-bar/headerBarPage.vue
@@ -4,7 +4,7 @@
 <template>
   <ul>
     <div class="nav_branch">
-      <router-link class="nav_items" to="/homePage">GO</router-link>
+      <router-link class="nav_items" to="/homePage">Githud_test</router-link>
     </div>
   </ul>
 </template>
