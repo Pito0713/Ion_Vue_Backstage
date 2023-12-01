d123aa1deffff1d8d3d9d367a4fef8773d414f51
Pito <xli30334@gmail.com>
2023-12-01 16:05:02 +0800

    local 新增dataLaist

--- src/pages/activePage/index.vue
+++ src/pages/activePage/index.vue
@@ -1,17 +1,39 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import { useRouter } from 'vue-router'
+import { dataLIST } from '../../configs/data'
 const router = useRouter()
+const page = ref(0)
 
+const handlePage = (done) => {
+  if (dataLIST.length >= page.value) page.value = page.value + 1
+}
+const List = reactive({
+  data: [],
+});
+const handleAnswer = (uesrValue) => {
+  List.data.push(uesrValue)
+  handlePage()
+}
 const handleClose = (done) => {
   router.push('/recordPage');
 }
 </script>
 
 <template>
-  <el-button type="primary" @click="handleClose">
-    Confirm
-  </el-button>
+  <template v-if="dataLIST.length > page">
+    <a>{{ dataLIST[page].question }}</a>
+    <template v-for="(option, index) in dataLIST[page].options" :key="index">
+      <el-button @click="handleAnswer(option[index].value)">
+        {{ option[index].text }}
+      </el-button>
+    </template>
+  </template>
+  <template v-else>
+    <el-button type="primary" @click="handlePage">
+      Confirm
+    </el-button>
+  </template>
 </template>
 
 <style lang="scss" scoped>
